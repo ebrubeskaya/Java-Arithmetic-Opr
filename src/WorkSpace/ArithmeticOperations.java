fc60e558f8d669bbc7eae73ffb83b2e006235e8c
ebrubeskaya <ebrubesk96@outlook.com>
2023-08-15 20:56:14 +0300

    operations

---
+++ src/WorkSpace/ArithmeticOperations.java
@@ -0,0 +1,32 @@
+package WorkSpace;
+
+public class ArithmeticOperations {
+
+    public static void main(String[] args ){
+
+        double point1=1.5;
+        double point2=2.3;
+        double point3=4.1;
+
+        double ort= (point1+point2+point3)/3;
+
+        System.out.println("Ortalama:"+ort);
+
+
+        int sayi=10;
+        System.out.println("İlk sayi:" +sayi);
+        System.out.println("İkinci  sayi:" + ++sayi);
+        System.out.println("Üçüncü sayi:" + sayi++);
+
+        int x = 7;
+        x/=3;
+        System.out.print();
+
+
+
+
+
+
+
+    }
+}
